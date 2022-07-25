de62602a9904310cee4056efe25e9d549ce246f7
Aussymo <aussymo@gmail.com>
2022-07-25 23:02:18 +0000

    my first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components';
-import { HashRouter as Router, Routes, Route}
+import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
 import Home from './pages';
 import Create from './pages/create';
